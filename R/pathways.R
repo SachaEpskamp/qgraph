f69e303f35bddd194528d18bf367a3fb2db22ac2
Sacha Epskamp <mail@sachaepskamp.com>
2016-10-25 13:15:16 +0200

    Added pathways function

---
+++ R/pathways.R
@@ -0,0 +1,44 @@
+# Function that highlights shortest paths in a network:
+
+pathways <- function(
+  graph,# Qgraph object
+  from, # Vector of from indices
+  to, # vector of to indices
+  fading = 0.25,
+  lty = 3
+){
+  stopifnot(is(graph,"qgraph"))
+  
+  # Character:
+  if (is.character(from)){
+    if (!all(from %in% graph$graphAttributes$Nodes$labels)){
+      stop("Node label in 'from' argument does not exist")
+    }
+    from <- match(from,  graph$graphAttributes$Nodes$labels)
+  }
+  
+  if (is.character(to)){
+    if (!all(to %in% graph$graphAttributes$Nodes$labels)){
+      stop("Node label in 'to' argument does not exist")
+    }
+    to <- match(to,  graph$graphAttributes$Nodes$labels)
+  }
+
+  
+  Cent <- centrality(graph,pkg = "igraph",all.shortest.paths = TRUE)
+  SP <- Cent$ShortestPaths
+  pathList <- matrix(NA,0,2)
+  for (i in from){
+    for (j in to){
+      pathList <- rbind(pathList,do.call(rbind,lapply(SP[[i,j]],function(x)cbind(x[-length(x)],x[-1]))))
+    }
+  }
+  
+  highlight <- rep(FALSE,nrow(pathList))
+  for (i in seq_len(nrow(pathList))){
+    highlight[i] <- which(graph$Edgelist$from %in% pathList[i,] & graph$Edgelist$to %in% pathList[i,])
+  }
+  graph$graphAttributes$Edges$color <- qgraph:::Fade(graph$graphAttributes$Edges$color, ifelse(seq_along(graph$Edgelist$from) %in% highlight, 1, fading))
+  graph$graphAttributes$Edges$lty <- ifelse(seq_along(graph$Edgelist$from) %in% highlight, 1, lty)
+  plot(graph)
+}
