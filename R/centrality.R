23bad1c1a6579fd47c3e96e0b923a9876cf564c9
AlexChristensen <32655669+AlexChristensen@users.noreply.github.com>
2018-05-17 12:11:35 -0400

    added RSPBC and hybrid centrality

--- R/centrality.R
+++ R/centrality.R
@@ -75,6 +75,11 @@ centrality <- function(graph,alpha=1,posfun=abs,pkg = c("igraph","qgraph"),all.s
   InExpectedInfluence <- colSums(W)
   OutExpectedInfluence <- rowSums(W)
   
+  # Randomized Shortest Paths Betweenness Centrality
+  rspbc <- NetworkToolbox::rspbc(W)
+  
+  # Hybrid Centrality
+  hyb <- NetworkToolbox::hybrid(W, BC = "random")
   
   DistMat <- 1/(ifelse(posfun(W)==0,0,posfun(W)^alpha))
   if (pkg=="igraph"){
@@ -217,6 +222,8 @@ centrality <- function(graph,alpha=1,posfun=abs,pkg = c("igraph","qgraph"),all.s
     InDegree = lab(CombinedDegreesIn,Labels),
     Closeness = lab(Closeness,Labels),
     Betweenness = lab(Betweenness,Labels),
+    RSPBC = lab(rspbc,Labels),
+    Hybrid = lab(hyb,Labels),
     InExpectedInfluence = InExpectedInfluence,
     OutExpectedInfluence = OutExpectedInfluence,
     ShortestPathLengths = lab(ShortestPaths,Labels),
