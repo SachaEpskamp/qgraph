31ee92f9f5f0ca070abe83169d2ada9361d6c331
Ubuntu User <ubuntu@packer-Ubuntu-18.04.2-Desktop>
2019-08-16 10:42:20 +0200

    added as.ggraph testversion

---
+++ R/as_ggraph.R
@@ -0,0 +1,51 @@
+as.ggraph <- function(object){
+  # To graph object using tidygraph:
+  df <- as.data.frame(object$Edgelist)
+  tidyG <- tidygraph::as_tbl_graph(df[,1:3], directed = all(df$directed))
+  
+  # Add ID factor:
+  tidyG <- tidyG %>% tidygraph::activate(edges) %>% dplyr::mutate(id = as.factor(seq_len(n())))
+  tidyG <- tidyG %>% tidygraph::activate(nodes) %>% dplyr::mutate(id = as.factor(seq_len(n())), label = object$graphAttributes$Nodes$labels)
+  
+  # Create plot:
+  ggraph::ggraph(tidyG, layout = object$layout) + 
+    
+    # Edges:
+    ggraph::geom_edge_fan(aes(
+      color = id,
+      edge_width = id,
+      edge_linetype = id
+    ), show.legend = FALSE) + 
+    ggraph::scale_edge_color_manual(values = object$graphAttributes$Edges$color) +
+    ggraph::scale_edge_width_manual(values = object$graphAttributes$Edges$width/2) +
+    ggraph::scale_edge_linetype_manual(values = sapply(object$graphAttributes$Edges$lty,switch,
+                                                       '1' = "solid", '2' = "dashed", '3'= "dotted", '4' = "dotdash", '5' = "longdash", '6' = "twodash"
+    )) + 
+    
+    # Nodes:
+    ggraph::geom_node_point(
+      aes(
+        size = id,
+        colour = id,
+        fill = id
+      ), shape = 21, show.legend = FALSE
+    ) + 
+    ggplot2::scale_size_manual(values = object$graphAttributes$Nodes$width*2)  + 
+    ggplot2::scale_colour_manual(values = object$graphAttributes$Nodes$border.color) +
+    ggplot2::scale_fill_manual(values = object$graphAttributes$Nodes$color) +
+    
+    # Labels:
+    ggraph::geom_node_text(
+      aes(
+        label = label
+      )
+    ) +
+    
+    # Limits:
+    ggplot2::xlim(-1-object$plotOptions$mar[2],1+object$plotOptions$mar[3]) + 
+    ggplot2::ylim(-1-object$plotOptions$mar[1],1+object$plotOptions$mar[3]) +
+    
+    # Theme
+    ggraph::theme_graph(plot_margin=margin(0,0,0,0))
+  
+}
