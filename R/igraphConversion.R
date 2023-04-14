7641432fd571b2e2ee2488b5c4008a48de3ee1ad
Sacha Epskamp <sacha.epskamp@gmail.com>
2023-04-14 10:27:11 +0800

    removed z-score warning ?centralityPlot

--- R/igraphConversion.R
+++ R/igraphConversion.R
@@ -1,22 +1,22 @@
 # library("igraph")
 
-as.igraph.qgraph <- function(object,attributes=TRUE)
+as.igraph.qgraph <- function(x,...,attributes=TRUE)
 {
-  if (!"qgraph"%in%class(object))
+  if (!"qgraph"%in%class(x))
   {
-    stop("Input must be qgraph object")
+    stop("Input must be qgraph x")
   }
 
   # Extract graph:
-  edgesort <- object$graphAttributes$Graph$edgesort
-  E <- as.matrix(as.data.frame(object$Edgelist[c("from","to")]))
+  edgesort <- x$graphAttributes$Graph$edgesort
+  E <- as.matrix(as.data.frame(x$Edgelist[c("from","to")]))
   E <- E[edgesort,,drop=FALSE]
   srt <- cbind(pmin(E[,1],E[,2]),pmax(E[,1],E[,2]))
-  Dir <- object$Edgelist$directed[edgesort]
-  Bi <-  object$Edgelist$bidirectional[edgesort]
+  Dir <- x$Edgelist$directed[edgesort]
+  Bi <-  x$Edgelist$bidirectional[edgesort]
   
   Graph <- graph.edgelist(E, any(Dir))
-  E(Graph)$weight <- object$Edgelist$weight[edgesort]
+  E(Graph)$weight <- x$Edgelist$weight[edgesort]
   
   # Arrow mode:
   aMode <- ifelse(Dir,2,0)
@@ -29,24 +29,24 @@ as.igraph.qgraph <- function(object,attributes=TRUE)
   if (attributes)
   {
     ## Node attributes:
-    V(Graph)$frame.color <- object$graphAttributes$Nodes$border.color
-    V(Graph)$frame.color[!object$graphAttributes$Nodes$borders] <- NA
-#     V(Graph)$label.cex <- object$graphAttributes$Nodes$label.cex
-    V(Graph)$label.color <- object$graphAttributes$Nodes$label.color
-    V(Graph)$label <- object$graphAttributes$Nodes$labels
-    V(Graph)$shape <- object$graphAttributes$Nodes$shape
+    V(Graph)$frame.color <- x$graphAttributes$Nodes$border.color
+    V(Graph)$frame.color[!x$graphAttributes$Nodes$borders] <- NA
+#     V(Graph)$label.cex <- x$graphAttributes$Nodes$label.cex
+    V(Graph)$label.color <- x$graphAttributes$Nodes$label.color
+    V(Graph)$label <- x$graphAttributes$Nodes$labels
+    V(Graph)$shape <- x$graphAttributes$Nodes$shape
     V(Graph)$shape[!V(Graph)$shape%in%c("circle", "square", "csquare", "rectangle", "crectangle", "vrectangle", "pie")] <- "none"
-    V(Graph)$color <- object$graphAttributes$Nodes$color
-    V(Graph)$size <- object$graphAttributes$Nodes$width / max((-1/72)*(object$nNodes)+5.35,1) * 4
-    V(Graph)$size2 <- object$graphAttributes$Nodes$height / max((-1/72)*(object$nNodes)+5.35,1) * 4
+    V(Graph)$color <- x$graphAttributes$Nodes$color
+    V(Graph)$size <- x$graphAttributes$Nodes$width / max((-1/72)*(x$nNodes)+5.35,1) * 4
+    V(Graph)$size2 <- x$graphAttributes$Nodes$height / max((-1/72)*(x$nNodes)+5.35,1) * 4
     
     ## Edge attributes:
-    E(Graph)$curved <- object$graphAttributes$Edges$curve[edgesort]
-    E(Graph)$color <- object$graphAttributes$Edges$color[edgesort]
-    if (is.character(object$graphAttributes$Edges$labels)) E(Graph)$label <- object$graphAttributes$Edges$labels[edgesort]
-    if (!is.null(object$graphAttributes$Edges$label.color)) E(Graph)$label.color <- object$graphAttributes$Edges$label.color[edgesort]
-    E(Graph)$lty <- object$graphAttributes$Edges$lty[edgesort]
-    E(Graph)$width <- object$graphAttributes$Edges$width[edgesort]
+    E(Graph)$curved <- x$graphAttributes$Edges$curve[edgesort]
+    E(Graph)$color <- x$graphAttributes$Edges$color[edgesort]
+    if (is.character(x$graphAttributes$Edges$labels)) E(Graph)$label <- x$graphAttributes$Edges$labels[edgesort]
+    if (!is.null(x$graphAttributes$Edges$label.color)) E(Graph)$label.color <- x$graphAttributes$Edges$label.color[edgesort]
+    E(Graph)$lty <- x$graphAttributes$Edges$lty[edgesort]
+    E(Graph)$width <- x$graphAttributes$Edges$width[edgesort]
   }
   
   return(Graph)
